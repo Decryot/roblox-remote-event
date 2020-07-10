1c73d8ec4bfb73b58c1f43c241494351f2744bfd
Dmitri <56200546+Decryot@users.noreply.github.com>
2020-07-10 13:58:36 -0400

    added 'arg'

--- robloxRemoteEvent/clientScript.lua
+++ robloxRemoteEvent/clientScript.lua
@@ -3,4 +3,6 @@ local player = game.Players.LocalPlayer;
 local replicatedStorage = game:GetService("ReplicatedStorage")
 local remoteEvent = replicatedStorage:WaitForChild("RemoteEvent")
 
+local arg = 0
+
 remoteEvent:FireServer(arg)
